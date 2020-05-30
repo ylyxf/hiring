5352de2b4a29c2361abeaa2d0505a617db6b407f
yulei <304189773@qq.com>
2020-05-30 17:58:01 +0800

    translate type to typeName using template

--- frontend-1-answer/web/src/components/CategoryPane.vue
+++ frontend-1-answer/web/src/components/CategoryPane.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <FileButton iconCls="icon-save" @select="importCategory">导入分类</FileButton>
-    <DataGrid :data="categoryList" style="height:250px">
+    <DataGrid idField="id" :data="categoryList" style="height:250px">
       <GridColumn field="name" title="Name"></GridColumn>
-      <GridColumn field="type" title="Type"></GridColumn>
+      <GridColumn field="type" title="Type">
+        <template slot="body" slot-scope="scope">{{scope.row.type==0?"支出":"收入"}}</template>
+      </GridColumn>
     </DataGrid>
   </div>
 </template>
@@ -19,16 +21,16 @@ export default {
   },
   methods: {
     importCategory: function(files) {
-      console.log('in method:',this.categoryList);
+      console.log("in method:", this.categoryList);
       // this can't be used in callback functions,defind _data to hold this
       let _data = this;
-      //Parse files[0] to json 
+      //Parse files[0] to json
       Papa.parse(files[0], {
         header: true,
-        complete: function(results,) {
+        complete: function(results) {
           console.log(results.data);
           //set json data to categoryList
-          _data.categoryList=results.data;
+          _data.categoryList = results.data;
         }
       });
     }
